3a3db9a1b7f542922ea8b930028498ca4e51c814
mimun433 <programewithmimun@gmail.com>
2026-07-20 17:20:11 +0600

    practice task

---
+++ practice.js
@@ -0,0 +1,163 @@
+// Task - 1
+let heading = 'Hello Javascript !';
+console.log(heading);
+let name = 'My name is Mimun.';
+console.log(name);
+let wish = 'I am exited to become A Web Developer.';
+console.log(wish);
+
+// 1.Addition Task 2
+
+let number1 = 25;
+let number2 = 10;
+let totalAdd = number1 + number2
+console.log(totalAdd)
+console.log('Addition:'+ totalAdd)
+
+// 2 Substraction
+
+let number1s = 25;
+let number2s = 10;
+let totalsubs = number1s - number2s;
+console.log(totalsubs);
+console.log('Substraction:'+ totalsubs);
+
+// 3.Multiplication
+
+let number1m = 25;
+let number2m = 10;
+let totalmul = number1m * number2m;
+console.log(totalmul);
+console.log('Multiplication:' + totalmul);
+
+// 4.Division
+
+let number1d = 25;
+let number2d = 10;
+let totaldiv = number1d / number2d;
+console.log(totaldiv);
+console.log('Division:'+ totaldiv)
+
+
+
+// 5. Moduluus
+
+let number1mod = 25;
+let number2mod = 10;
+let totalmodu = number1mod % number2mod;
+
+console.log(totalmodu);
+console.log('Moduluus:'+ totalmodu);
+
+// Task 3.student information.
+
+let studentName = 'Mahmud Hassan Mimun';
+console.log(studentName);
+console.log('Student Name : ' + studentName);
+let age = 20;
+console.log( 'Age : ' + age);
+let country = "bangladesh";
+let isStudent = true;
+
+// 4.identify data types
+
+let a = 100;
+console.log('a:'+typeof a);
+let b = "programming"
+console.log('b:'+typeof b);
+let c = true;
+console.log('c:'+typeof c);
+let d = false;
+console.log('d:'+typeof d);
+let e = 3.1416;
+console.log('e:'+typeof e);
+
+// Task 5.
+let firstName_ = 'Mahmud Hassan';
+let First_Name = 'Mimun';
+let user123 = 1;
+let $price = 19.99;
+let _total = 0;
+console.log(firstName_,First_Name,user123,$price,_total,);
+//6 Personal Bio
+
+let head = 'Hello !'
+console.log(head);
+let NAme = 'Mimun'
+console.log('My name is ' +  NAme);
+let AGe = 26;
+console.log('I am ' +AGe+ 'years old. ')
+let favoriteFood = "Pizza";
+console.log('My favorite food is ' + favoriteFood);
+let favoriteLanguage = "JavaScript";
+console.log('My favorite programming language is ' + favoriteLanguage);
+let loveCoding = true;
+console.log('I love coding ' + loveCoding);
+
+// 7.number  add
+
+let number7_1 = 50;
+console.log('Number 1 :'+ number7_1)
+let number7_2 = 8;
+console.log('Number 2 :'+ number7_2)
+let Addition7 = (number7_1 + number7_2);
+console.log('Addition :' + number7_1)
+let Substraction7 = (number7_1 - number7_2);
+console.log('Substraction :'+ Substraction7);
+let Multiplication7 = (number7_1 * number7_2);
+console.log('Multiplication :' + Multiplication7);
+let Division7 = (number7_1 / number7_2);
+console.log("division :"+ Division7);
+let Modulus7 = (number7_1 % number7_2) ;
+console.log('Modulus :' + Modulus7);
+
+// task 8.Shorthand Operators(assign adding)
+let score =  50;
+console.log(score);
+score += 10;
+console.log('after +=10: ' + score);
+score -= 5;
+console.log('after -= 5: ' + score);
+score *= 2;
+console.log('after *= 2: ' + score);
+score /= 5;
+console.log('after /= 5: ' + score);
+score %= 4;
+console.log('after %= 4 : ' + score);
+
+//Mini coding challenge 
+let price = 200;
+let quantity = 3;
+let discount = 10;
+let totalPrice = (price * quantity);  
+console.log('Total Price  :  ' + totalPrice);
+let discountAmount = (totalPrice * discount) / 100;
+console.log('Discount Amount : '+ discountAmount);
+let finalPrice = (totalPrice - discountAmount);
+console.log('Final Price : ' + finalPrice);
+//10.console quize 
+
+let x = 20;
+let y = 5;
+console.log(x+y);
+
+let language = 'Javascript';
+console.log(typeof language);
+
+let isOnline = false;
+console.log(typeof isOnline);
+
+let num = 10;
+console.log(num);
+
+
+
+
+
+
+
+
+
+
+
+
